587572f6717c08febfd7bfd6367d46980b961a05
JinGoon-Kim <raneata@gmail.com>
2021-07-31 23:05:52 +0900

    finish

--- js/greeting.js
+++ js/greeting.js
@@ -7,7 +7,7 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(e) {
     e.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username)
@@ -17,8 +17,6 @@ function paintGreetings(username) {
     greeting.innerText = `Hello ${savedUsername}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
